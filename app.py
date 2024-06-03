3a80a46811034b4ac4eba99b4bae418d65facd6b
mkleinegger@gmail.com <mkleinegger@gmail.com>
2024-06-03 15:47:19 +0200

    add basic local impl

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import base64
 import json
 import os
-import time
 import cv2 as cv
 import numpy as np
 from flask import Flask, request
 
 app = Flask(__name__)
-net = None
 
+net = None
 class_names = []
+
+CLASSIFICATION_THRESHOLD = 0.5
 
 YOLO_CONFIG = os.path.abspath("./yolo_configs/yolov3-tiny.cfg")
 YOLO_WEIGHTS = os.path.abspath("./yolo_configs/yolov3-tiny.weights")
@@ -19,16 +20,18 @@ YOLO_CLASS_NAMES = os.path.abspath("./yolo_configs/coco.names")
 # -----------------------------functions-------------------------------
 # ---------------------------------------------------------------------
 
+
 def setup():
     global class_names, net
     # Load the COCO class labels
-    with open(YOLO_CLASS_NAMES, 'r') as f:
-        class_names = f.read().strip().split('\n')
+    with open(YOLO_CLASS_NAMES, "r") as f:
+        class_names = f.read().strip().split("\n")
 
     # Load the YOLOv3-tiny network
     net = cv.dnn.readNetFromDarknet(YOLO_CONFIG, YOLO_WEIGHTS)
     net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
     net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
 
 def object_detection(id, image_data):
     def convert_base64_to_image(image_data):
@@ -36,60 +39,52 @@ def object_detection(id, image_data):
         image = cv.imdecode(nparr, cv.IMREAD_COLOR)
         return image
 
+    def get_output_layer_names():
+        layer_names = net.getLayerNames()
+        unconnected_out_layers = net.getUnconnectedOutLayers()
+        if len(unconnected_out_layers.shape) > 1:
+            unconnected_out_layers = unconnected_out_layers.flatten()
+        return [layer_names[i - 1] for i in unconnected_out_layers]
+
     image = convert_base64_to_image(image_data)
     (H, W) = image.shape[:2]
 
-    layer_names = net.getLayerNames()
-    unconnected_out_layers = net.getUnconnectedOutLayers()
-    if len(unconnected_out_layers.shape) > 1:
-        unconnected_out_layers = unconnected_out_layers.flatten()
+    output_layer_names = get_output_layer_names()
 
-    output_layer_names = [layer_names[i - 1] for i in unconnected_out_layers]
+    blob = cv.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
 
-    blob = cv.dnn.blobFromImage(image, 1/255.0, (416, 416), swapRB=True, crop=False)
     net.setInput(blob)
+
     layer_outputs = net.forward(output_layer_names)
 
-    boxes = []
-    confidences = []
-    class_ids = []
-
+    # We still need to calculate boxes, to simply remove overlapps afterwards
+    boxes, confidences, class_ids = [], [], []
     for output in layer_outputs:
         for detection in output:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
 
-            if confidence > 0.5:
+            if confidence > CLASSIFICATION_THRESHOLD:
                 box = detection[0:4] * np.array([W, H, W, H])
-                (centerX, centerY, width, height) = box.astype('int')
+                (centerX, centerY, width, height) = box.astype("int")
 
-                # Use the center (x, y)-coordinates to derive the top-left corner of the bounding box
-                x = int(centerX - (width / 2))
-                y = int(centerY - (height / 2))
+                x, y = int(centerX - (width / 2)), int(centerY - (height / 2))
 
-                # Update the lists of bounding box coordinates, confidences, and class IDs
                 boxes.append([x, y, int(width), int(height)])
 
                 confidences.append(float(confidence))
                 class_ids.append(class_id)
 
-    # Apply non-maxima suppression to suppress weak, overlapping bounding boxes
     indices = cv.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 
     results = {}
     if len(indices) > 0:
         for i in indices.flatten():
-            label = class_names[class_ids[i]]
-            confidence = confidences[i]
+            label, confidence = class_names[class_ids[i]], confidences[i]
+            results[label] = confidence
 
-            results[label] =  confidence
-
-    # Print the results
-    print("-"*50)
-    for label, score in results.items():
-        print(f"Object: {label}, Confidence: {score:.4f}")
-    print("-"*50)
+    return results
 
 
 # ---------------------------------------------------------------------
@@ -97,24 +92,23 @@ def object_detection(id, image_data):
 # ---------------------------------------------------------------------
 
 
-@app.route("/object-detection", methods=["POST"])
+@app.route("/object_detection", methods=["POST"])
 def json_response():
     body = json.loads(request.get_json())
-    id, image_data = body['id'], body['image_data']
-    
-    object_detection(id, image_data)
+    result = object_detection(body["id"], body["image_data"])
 
     return json.dumps(
         {
-            "id": "2b7082f5-d31a-54b7-a46e-5e4889bf69bd",
+            "id": body["id"],
             "objects": [
-                {"label": "table", "accuracy": 0.790481352806091},
-                {"label": "person", "accuracy": 0.7877481352806091},
+                {"label": label, "accuracy": accuracy}
+                for label, accuracy in result.items()
             ],
         }
     )
 
 
+# basic route to ensure API is live
 @app.route("/")
 def hello():
     return "Hello, World!"
@@ -122,4 +116,4 @@ def hello():
 
 if __name__ == "__main__":
     setup()
-    app.run(debug=True, port=8000)
+    app.run()
--- client.py
+++ client.py
@@ -1,4 +1,3 @@
-import argparse
 import json
 import os
 import sys
@@ -6,6 +5,7 @@ import requests
 import glob
 import uuid
 import base64
+
 
 def main():
     if len(sys.argv) != 3:
@@ -20,21 +20,32 @@ def main():
         with open(file, "rb") as f:
             encoded_string = base64.b64encode(f.read())
             file_uuid = uuid.uuid4()
-            
-            print(f"Requesting object-detection for image '{os.path.basename(file)}' with id '{file_uuid}'")
-            response = requests.post(endpoint, json=json.dumps({
-                "id": str(file_uuid),
-                "image_data": encoded_string.decode('utf-8')
-            }))
-            
+
+            print(
+                f"Requesting object-detection for image '{os.path.basename(file)}' with id '{file_uuid}'"
+            )
+            response = requests.post(
+                endpoint,
+                headers={"Content-type": "application/json"},
+                json=json.dumps(
+                    {"id": str(file_uuid), "image_data": encoded_string.decode("utf-8")}
+                ),
+            )
+
             if response.status_code != 200:
                 print(f"-> Couldn't receive results for image with id '{file_uuid}'")
                 continue
-            
+
             result = response.json()
             print(f"-> Received following objects for id '{result['id']}':")
-            for o in result['objects']:
-                print(f"   Object '{o['label']}' found with accuracy {o['accuracy']:.4f}")
+            if len(result["objects"]) == 0:
+                print(f"   No objects could be detected!")
+            else:
+                for o in result["objects"]:
+                    print(
+                        f"   Object '{o['label']}' found with accuracy {o['accuracy']:.4f}"
+                    )
+
 
 if __name__ == "__main__":
     main()
