2634ab1e523248fd85b179ac2a302e83d23bc005
mkleinegger@gmail.com <mkleinegger@gmail.com>
2024-06-03 12:33:53 +0200

    basic client impl

---
+++ client.py
@@ -0,0 +1,40 @@
+import argparse
+import json
+import os
+import sys
+import requests
+import glob
+import uuid
+import base64
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python client.py <inputfolder> <endpoint>")
+        sys.exit(1)
+
+    input_folder = sys.argv[1]
+    endpoint = sys.argv[2]
+
+    path = os.path.abspath(input_folder)
+    for file in glob.glob(f"{path}/*.*"):
+        with open(file, "rb") as f:
+            encoded_string = base64.b64encode(f.read())
+            file_uuid = uuid.uuid4()
+            
+            print(f"Requesting object-detection for image '{os.path.basename(file)}' with id '{file_uuid}'")
+            response = requests.post(endpoint, json=json.dumps({
+                "id": str(file_uuid),
+                "image_data": encoded_string.decode('utf-8')
+            }))
+            
+            if response.status_code != 200:
+                print(f"-> Couldn't receive results for image with id '{file_uuid}'")
+                continue
+            
+            result = response.json()
+            print(f"-> Received following objects for id '{result['id']}':")
+            for o in result['objects']:
+                print(f"   Object '{o['label']}' found with accuracy {o['accuracy']:.4f}")
+
+if __name__ == "__main__":
+    main()
