5e840d8e6994270c75f2f500919fad5cdb8409ca
allorenz <alexander.lorenz99@gmail.com>
2024-06-16 13:30:38 +0200

    aws implementation

---
+++ src/aws_lambda_function.py
@@ -0,0 +1,31 @@
+
+import json
+import boto3
+from yolo_model import load_image_as_base64, load_model, object_detection
+
+
+s3_client = boto3.client("s3")
+
+
+def lambda_handler(event, context):
+    # get bucket and key name
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = event["Records"][0]["s3"]["object"]["key"]
+    
+    print("Load Image")
+    # get object and read image
+    response = s3_client.get_object(Bucket=bucket, Key=key)
+    image_content = response["Body"].read()
+    
+    # convert image to base64
+    img_data, file_uuid = load_image_as_base64(image_content)
+    
+    
+    print("Load Model")
+    net = load_model(s3_client)
+    
+    print("Do object detection")
+    result = object_detection(file_uuid, img_data, net)
+    
+    print(result)
+    print("Done")
